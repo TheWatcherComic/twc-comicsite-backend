9a5b5d75508e15d48611d1ec98a917550ff10bb7
Gabriel Rodriguez <gabrielhudgins26@gmail.com>
2022-07-21 23:32:58 -0500

    refactor confirmacion de pago redirect

--- src/controllers/payment/yappy.controller.js
+++ src/controllers/payment/yappy.controller.js
@@ -12,10 +12,12 @@ class YappyController {
         }
     }
     async confirmPayment(req, res) {
-        console.log("Request " + req.params);
-        const data = req.params;
-        console.log("Data" + JSON.stringify(data));
-        return res.status(301).redirect("https://twc-comicsite-frontend.vercel.app")
+        try {
+            console.log("Data" + JSON.stringify(req.params));
+            return res.status(301).redirect("https://twc-comicsite-frontend.vercel.app");
+        } catch (err) {
+            res.status(500).redirect("https://twc-comicsite-frontend.vercel.app");
+        }
     }
 }
 module.exports = YappyController;
