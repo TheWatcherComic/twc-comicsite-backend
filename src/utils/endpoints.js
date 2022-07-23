16b8229e801428cf382a5ef819ac691746ee86e4
Johan Infante <johaninft21@gmail.com>
2022-07-23 12:32:52 -0500

    change endpoint

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -2,10 +2,7 @@
 const BASE_ENDPOINT = "/api";
 const YAPPY_ENDPOINT = BASE_ENDPOINT + "/pagosbg";
 const ALLCOMICS_ENDPOINT = BASE_ENDPOINT + "/comics";
-<<<<<<< HEAD
 const COMICS_ENDPOINT = BASE_ENDPOINT + "/comicData";
-=======
->>>>>>> 3fe7e259580089148088248b43101930833ab3ae
 
 /**External endpoints */
 
