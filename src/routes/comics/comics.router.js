1315d0c345e12129743e50ecc7a3c9283f7f3af7
Johan Infante <johaninft21@gmail.com>
2022-07-27 12:43:33 -0500

    delete DC & MARVEL api

--- src/routes/comics/comics.router.js
+++ src/routes/comics/comics.router.js
@@ -9,8 +9,6 @@ const router = Router()
 .post(STAGECOMICS_ENDPOINT, controllerInstance.stageComicsData)
 .post(COMICS_ENDPOINT, controllerInstance.comicInfoController)
 .post(ALLCOMICS_ENDPOINT, checkIfAuthenticated, controllerInstance.userComicsController)
-.get(ALLCOMICS_ENDPOINT + "/marvel" ,  controllerInstance.getAllComicsData)
-.get(ALLCOMICS_ENDPOINT + "/dc" ,  controllerInstance.getAllComicsData);
 
 
 module.exports = router;
