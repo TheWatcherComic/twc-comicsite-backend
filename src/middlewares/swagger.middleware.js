8c86104e6d682259511613e45c61f9e59dc5d190
Gabriel Rodriguez <gabrielhudgins26@gmail.com>
2022-07-28 00:46:26 -0500

    Agregada documentacion

--- src/middlewares/swagger.middleware.js
+++ src/middlewares/swagger.middleware.js
@@ -10,8 +10,6 @@ const swagger = (app, routes) => {
       if (err) {
         return reject(err);
       }
-      // Enable Express' case-sensitive and strict options
-      // (so "/entities", "/Entities", and "/Entities/" are all different)
       app.enable('case sensitive routing');
       app.enable('strict routing');
 
@@ -19,39 +17,31 @@ const swagger = (app, routes) => {
       app.use(
         mw.files(
           {
-            // Override the Express App's case-sensitive
-            // and strict-routing settings for the Files middleware.
             caseSensitive: false,
             strict: false,
           },
           {
             useBasePath: false,
             apiPath: process.env.SWAGGER_API_SPEC,
-            // Disable serving the "api.yml" file
-            // rawFilesPath: false
           }
         )
       );
 
       app.use(
         mw.parseRequest({
-          // Configure the cookie parser to use secure cookies
           cookie: {
             secret: process.env.SESSION_SECRET,
           },
-          // Don't allow JSON content over 100kb (default is 1mb)
           json: {
             limit: process.env.REQUEST_LIMIT,
           },
         })
       );
-      // These two middleware don't have any options (yet)
       app.use(mw.CORS(), mw.validateRequest());
 
       routes(app);
 
       app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-      // eslint-disable-next-line no-unused-vars, no-shadow
       app.use(errorHandler);
       return resolve();
     });
--- src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-const pino = require('pino');
-
-const log = pino({
-  name: process.env.APP_ID,
-  level: process.env.LOG_LEVEL,
-});
-
-module.exports = log;
